7a94a8d849399cc502e2d00cedfbb1602ab522da
Edison Ade <ask@buzzedison.com>
2019-05-07 01:58:12 +0100

    about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,13 +1,33 @@
 import React from "react"
-import { MDBJumbotron, MDBBtn, MDBContainer, MDBRow, MDBCol } from "mdbreact"
-
+import {
+  MDBJumbotron,
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCarousel,
+  MDBCarouselItem,
+  MDBView,
+  MDBMask,
+  MDBCarouselInner,
+} from "mdbreact"
+import DELA from "../images/267.jpg"
+import DELA2 from "../images/269.jpg"
+import MEDIA from "../images/267.jpg"
 const About = () => {
   return (
     <MDBContainer className="mt-5 text-center" fluid>
       <MDBRow>
-        <MDBCol>
-          <MDBJumbotron style={{ backgroundColor: "#ffffff" }}>
-            <h2 className="h1 display-4">
+        <MDBCol md={6}>
+          <MDBJumbotron style={{ backgroundColor: "#FE9C54  " }}>
+            <h2
+              className="h1 display-5"
+              style={{
+                paddingTop: "40px",
+                color: "white",
+                fontWeight: "800px",
+              }}
+            >
               A Full Service 360 Degree Branding and Advertising Agency
             </h2>
             <p className="lead">
@@ -15,8 +35,10 @@ const About = () => {
             </p>
             <hr className="my-2" />
             <p>
-              A good creative is not just good business; it's the human touch
-              that brings people together.
+              <strong>
+                A good creative is not just good business; it's the human touch
+                that brings people together.
+              </strong>
             </p>
             <p className="lead">
               <MDBBtn lg color="dark">
@@ -24,6 +46,51 @@ const About = () => {
               </MDBBtn>
             </p>
           </MDBJumbotron>
+        </MDBCol>
+        <MDBCol sm={6}>
+          <MDBCarousel
+            activeItem={1}
+            length={3}
+            showControls={true}
+            showIndicators={true}
+            className="z-depth-1"
+          >
+            <MDBCarouselInner>
+              <MDBCarouselItem itemId="1">
+                <MDBView>
+                  <img
+                    className="d-block w-200"
+                    src={DELA}
+                    alt="First slide"
+                    style={{ width: "100%" }}
+                  />
+                  <MDBMask overlay="black-light" />
+                </MDBView>
+              </MDBCarouselItem>
+              <MDBCarouselItem itemId="2">
+                <MDBView>
+                  <img
+                    className="d-block w-200"
+                    src={DELA2}
+                    alt="Second slide"
+                    style={{ width: "100%" }}
+                  />
+                  <MDBMask overlay="black-strong" />
+                </MDBView>
+              </MDBCarouselItem>
+              <MDBCarouselItem itemId="3">
+                <MDBView>
+                  <img
+                    className="d-block w-200"
+                    src={MEDIA}
+                    alt="Third slide"
+                    style={{ width: "100%" }}
+                  />
+                  <MDBMask overlay="black-slight" />
+                </MDBView>
+              </MDBCarouselItem>
+            </MDBCarouselInner>
+          </MDBCarousel>
         </MDBCol>
       </MDBRow>
     </MDBContainer>
